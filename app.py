87c7c30c9ece85b1182ef365445148571a666aa4
fatimatts970 <anjaanshahzadi27@gmail.com>
2026-07-18 22:56:00 +0500

    app.py

--- app.py
+++ app.py
@@ -112,5 +112,18 @@ def terms():
 @app.route("/contact")
 def contact():
     return send_from_directory(HTML_DIR, "contact.html")
+
+@app.route("/robots.txt")
+def robots():
+    return send_from_directory(HTML_DIR, "robots.txt")
+
+@app.route("/sitemap.xml")
+def sitemap():
+    return send_from_directory(HTML_DIR, "sitemap.xml")
+
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(HTML_DIR, "favicon.ico")
+
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=PORT)
